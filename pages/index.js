b2569eb6573b65e338c7697b8fb460cb85eca140
chrysanthemum <asingiser@gmail.com>
2023-08-10 12:14:58 -0400

    create Property component, delete favicon, map property data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
-import { baseUrl, fetchApi } from '../utils/fetchApi';
+import { baseUrl, fetchApi } from '../utils/fetchApi'; 
+import Property from '../components/Property';
 
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
@@ -35,7 +36,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
                 imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
             />
             <Flex flexWrap="wrap">
-                {/* Fetch rental properties and map them */}
+                {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
             </Flex>
             <Banner 
                 purpose="Buy A HOME"
@@ -47,7 +48,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
                 linkName="/search?purpose=for-sale"
                 imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
             />
-                {/* Fetch for sale properties and map them */}
+                {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
         </Box>
     )
 }
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com/auto-complete'
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers : {
-            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Key': '280decefc9msh6351b84dfa69897p1f25b2jsnece4de295cb3',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     })
