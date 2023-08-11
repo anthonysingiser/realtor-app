9852731bac3dc61831450a8f32b8b94968604038
chrysanthemum <asingiser@gmail.com>
2023-08-11 12:23:45 -0400

    add to Property component, adjust _app layout, create Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,24 @@ import Head from 'next/head';
 import NProgress from 'nprogress';
 import { ChakraProvider } from '@chakra-ui/react';
 import Home from '.';
+import Layout from '../components/Layout';
+
 
 function MyApp({ Component, pageProps }) {
-  NProgress.configure({ showSpinner: false });
-
-  Router.events.on('routeChangeStart', () => {
-    NProgress.start();
-  });
-
-  Router.events.on('routeChangeComplete', () => {
-    NProgress.done();
-  });
-
   return (
     <>
       <Head>
+
       </Head>
       <ChakraProvider>
-        <Home />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>    
       </ChakraProvider>
+
     </>
-  );
-}
+  )
+
+};
 
 export default MyApp;
