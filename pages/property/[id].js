a7c5d617fb5ef57258824c4a844561c44b829f03
chrysanthemum <asingiser@gmail.com>
2023-08-21 10:55:45 -0400

    add amenities to property details page

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -47,7 +47,25 @@ const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title
                     </Flex>
                 )}
             </Flex>
-            
+            <Box>
+                {amenities.length && <Text fontSize="2xl" fontWeight="black" marginTop="5">Amenities</Text>}
+                    <Flex flexWrap="wrap">
+                        {amenities.map((item) => (
+                            item.amenities.map((amenity) => (
+                                <Text 
+                                    fontWeight="bold"
+                                    color="blue.400"
+                                    fontSize="1"
+                                    p="2"
+                                    bg="gray.200"
+                                    m="1"
+                                    borderRadius="5"
+                                    key={amenity.text}
+                                >{amenity.text}</Text>
+                            ))
+                        ))}
+                    </Flex>
+            </Box>
         </Box>
     </Box>
 )
