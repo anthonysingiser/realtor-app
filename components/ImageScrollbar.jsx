93e596fd8d44308aa097838cedc5fd4c473cffbb
chrysanthemum <asingiser@gmail.com>
2023-08-26 11:47:42 -0400

    reworking scrollbar

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import Image from 'next/image';
 import { Box, Icon, Flex } from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { FaArrowAltCircleLeft, FaArrorwAltCircleRight } from 'react-icons';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
 const LeftArrow= () => {
     const { scrollPrev } = useContext(VisibilityContext)
@@ -14,7 +14,6 @@ const LeftArrow= () => {
             onClick={() => scrollPrev()}
             fontSize="2xl"
             cursor="pointer"
-            d={['none', 'none', 'none', 'block']}
         />
     </Flex>
 
@@ -25,37 +24,34 @@ const RightArrow= () => {
     const { scrollNext } = useContext(VisibilityContext)
 
     return (
-    <Flex justifyContent="center" alignItems="center" marginRight="1">
+    <Flex justifyContent="center" alignItems="center" marginLeft="1">
         <Icon
-            as={FaArrorwAltCircleRight}
+            as={FaArrowAltCircleRight}
             onClick={() => scrollNext()}
             fontSize="2xl"
             cursor="pointer"
-            d={['none', 'none', 'none', 'block']}
         />
     </Flex>
     )
 }
 
-const ImageScrollbar = ({ data }) => {
+export default function ImageScrollbar ({ data }) {
 
     return (
-        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden'}}>
+        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }}>
             {data.map((item) => (
-                <Box key={item.id} width='910px' itemID={item.id} overflow='hidden' p='1'>
+                <Box width='910px' itemID={item.id} overflow='hidden' p='1' key={item.id}>
                     <Image
-                        alt='image'
+                        alt='property image'
                         placeholder='blur'
                         blurDataURL={item.url}
                         src={item.url}
                         width={1000}
                         height={500}
-                        sizes= '(max-width:500px) 100px, (max-width):1023px 400px, 1000px'
+                        sizes='(max-width:500px) 100px, (max-width):1024px 400px, 1000px'
                     />
                 </Box>
             ))}
         </ ScrollMenu>
     )
 }
-
-export default ImageScrollbar
